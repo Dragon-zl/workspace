418edc15146d9b530c7228b3228363a62d085a59
dzl <dzl@localhost.localdomain>
2022-01-09 23:08:27 +0800

    1.9

--- nowcoder/第五章_项目实战/webserver_http/log.c
+++ nowcoder/第五章_项目实战/webserver_http/log.c
@@ -1,13 +1,46 @@
-//#include "log.h"
+#include "log.h"
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdarg.h>
+#include <string>
+int Log :: My_close_log = 1;
+void * flush_log_thread(void * arg){
 
-/*
+}
 bool Log :: init(const char *file_name, int close_log, 
             int log_buf_size, int split_lines, 
             int max_queue_size){
     //判断 如果设置了 max_queue_size ,则设置为异步
     if(max_queue_size >= 1){
         My_is_async = true;
+        My_log_queue = new blockqueue<string>(max_queue_size);
+        pthread_t  tid;
+        //flush_log_thread 为回调函数，这里表示创建线程异步写日志
+        pthread_create(&tid , NULL , flush_log_thread , NULL);
     }
     
+    My_close_log    = close_log;
+    //设置日志缓冲区大小
+    My_log_buf_size = log_buf_size;
+    My_buf  =  new char[My_log_buf_size];
+    //格式化字符串： 全部置 0 
+    memset( My_buf , '\0' , My_log_buf_size);
+    //设置日志的最大行数
+    My_split_lines = split_lines;
+
+    time_t  t = time(NULL);
+    //获取实时时间
+    struct  tm  * sys_tm = localtime(&t);
+    struct  tm  My_tm = *sys_tm;
+
+    const  char *p = strstr(file_name , '/');
+    char log_full_name[256] = {0};
+
+    if( p == NULL){
+        //格式化字符串：文件名
+        snprintf( log_full_name , 255 , "%d_%02d_%02d_%s" , My_tm.tm_year + 1900 ,
+                    My_tm.tm_mon + 1 , My_tm.tm_mday , file_name);
+        
+    }
 }
-*/
--- nowcoder/第五章_项目实战/webserver_http/log.h
+++ nowcoder/第五章_项目实战/webserver_http/log.h
@@ -1,5 +1,50 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include "blockqueue.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class Log{
+
+    public:
+    static Log * get_instance(){
+        static Log instance;
+        return &instance;
+    }
+    static void * flush_log_thread(void *args){
+        Log :: get_instance()->async_write_log();
+    }
+    //可选择的参数有日志文件、日志缓冲区大小、最大行数以及最长日志条队列
+    bool init(const char *file_name, int close_log, 
+            int log_buf_size = 8192, int split_lines = 5000000, 
+            int max_queue_size = 0);
+
+    private:
+    locker My_mutex;            //互斥锁
+    FILE * My_fp;               //打开log的文件指针
+    static int My_close_log;    //关闭日志
+    int My_log_buf_size;        //日志缓冲区大小
+    char *  My_buf;             
+    int My_split_lines;         //日志最大行数
+    blockqueue<string> * My_log_queue; //阻塞队列
+    void * async_write_log(){
+        string single_log;
+        //从阻塞队列中取出一个日志 string ，写入文件
+        while( My_log_queue -> pop(single_log)){
+            //出队
+            //加锁
+            My_mutex.lock();
+            fputs( single_log.c_str() , My_fp);
+            My_mutex.unlock();
+        }
+    }
+};
 
 
+
+#endif
 
 
 
